272879356805dc5ac33eead6680c5f8208193df0
Sragvi <reachsragvi@gmail.com>
2019-05-21 06:29:35 +0000

    First Commit

---
+++ java.js
@@ -0,0 +1,12 @@
+var i = 0;
+var txt = 'Sragvi Vadali\'s Engineering Portfolio'; /* The text */
+var speed = 150; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("header").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+window.onload = typeWriter
